5ec8489374eee08188b7505d160b4abc73f1b7e1
NeelaReddy <neelareddy.i25@gmail.com>
2024-05-10 06:54:09 +0530

    working hard

---
+++ for-each/variables.tf
@@ -0,0 +1,26 @@
+variable instance_names {
+    type = map 
+    default = {
+        db = "t3.small"
+        backend = "t3.micro"
+        frontend = "t3.micro"
+    }
+}
+
+variable "common_tags" {
+    type = map 
+    default = {
+        project = "expense"
+        Terraform = "true"
+    }
+}
+
+variable "domain_name" {
+    default = "neelareddy.store"
+}
+
+variable "zone_id" {
+    default = "Z02253671OIE92FOK6QEZ"
+}
+
+
